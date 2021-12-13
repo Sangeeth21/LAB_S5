662ca34afcb40e144f85990dd116664d0da83266
SANGEETH SUBHASH <84234657+Sangeeth21@users.noreply.github.com>
2021-12-13 20:55:28 +0530

    Update Linked_file_allocation.c

--- Linked_file_allocation.c
+++ Linked_file_allocation.c
@@ -1,44 +1,40 @@
-#include <stdio.h>
-#include <stdlib.h>
-void recursivePart(int pages[]){
-    int st, len, k, c, j;
-    printf("Enter the index of the starting block and its length: ");
-    scanf("%d%d", &st, &len);
-    k = len;
-    if (pages[st] == 0){
-        for (j = st; j < (st + k); j++){
-            if (pages[j] == 0){
-                pages[j] = 1;
-                printf("%d------>%d\n", j, pages[j]);
-            }
-            else {
-                printf("The block %d is already allocated \n", j);
-                k++;
-            }
-        }
-    }
-    else
-        printf("The block %d is already allocated \n", st);
-    printf("Do you want to enter more files? \n");
-    printf("Enter 1 for Yes, Enter 0 for No: ");
-    scanf("%d", &c);
-    if (c==1)
-        recursivePart(pages);
-    else
-        exit(0);
-    return;
+#include<stdio.h>
+#include<stdlib.h>
+void main()
+{
+int f[50], p,i, st, len, j, c, k, a;
+for(i=0;i<50;i++)
+f[i]=0;
+printf("Enter how many blocks already allocated: ");
+scanf("%d",&p);
+printf("Enter blocks already allocated: ");
+for(i=0;i<p;i++)
+{
+scanf("%d",&a);
+f[a]=1;
 }
-int main(){
-    int pages[50], p, a;
-    for (int i = 0; i < 50; i++)
-        pages[i] = 0;
-    printf("Enter the number of blocks already allocated: ");
-    scanf("%d", &p);
-    printf("Enter the blocks already allocated: ");
-    for (int i = 0; i < p; i++){
-        scanf("%d", &a);
-        pages[a] = 1;
-    }
-    recursivePart(pages);
-    return 0;
+x: printf("Enter index starting block and length: ");
+scanf("%d%d", &st,&len);
+k=len;
+if(f[st]==0)
+{
+for(j=st;j<(st+k);j++)
+{
+if(f[j]==0)
+{
+f[j]=1;
+printf("%d-------->%d\n",j,f[j]);
 }
+else
+{
+printf("%d Block is already allocated \n",j);
+k++;
+}
+}
+}
+else
+printf("%d starting block is already allocated \n",st);
+printf("Do you want to enter more file(Yes - 1/No - 0)");
+scanf("%d", &c);
+if(c==1)
+goto x;}

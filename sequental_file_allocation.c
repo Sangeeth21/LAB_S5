3303d345bb6d9019a649a18fff0af1b53a0630cb
SANGEETH SUBHASH <84234657+Sangeeth21@users.noreply.github.com>
2021-12-13 20:40:08 +0530

    Update sequental_file_allocation.c

--- sequental_file_allocation.c
+++ sequental_file_allocation.c
@@ -1,40 +1,31 @@
-#include <stdio.h>
-#include <stdlib.h>
-void recurse(int files[]){
-    int flag = 0, startBlock, len, j, k, ch;
-    printf("Enter the starting block and the length of the files: ");
-    scanf("%d%d", &startBlock, &len);
-    for (j=startBlock; j<(startBlock+len); j++){
-        if (files[j] == 0)
-            flag++;
-    }
-    if(len == flag){
-        for (int k=startBlock; k<(startBlock+len); k++){
-            if (files[k] == 0){
-                files[k] = 1;
-                printf("%d\t%d\n", k, files[k]);
-            }
-        }
-        if (k != (startBlock+len-1))
-            printf("The file is allocated to the disk\n");
-    }
-    else
-        printf("The file is not allocated to the disk\n");
-    printf("Do you want to enter more files?\n");
-    printf("Press 1 for YES, 0 for NO: ");
-    scanf("%d", &ch);
-    if (ch == 1)
-        recurse(files);
-    else
-        exit(0);
-    return;
-}
-int main()
+#include<stdio.h>
+void main()
 {
-int files[50];
-for(int i=0;i<50;i++)
-files[i]=0;
-printf("Files Allocated are :\n");
-recurse(files);
-return 0;
+int f[50], i, st, len, j, c, k, count = 0;
+for(i=0;i<50;i++)
+f[i]=0;
+printf("Files Allocated are : \n");
+x: count=0;
+printf("Enter starting block and length of files: ");
+scanf("%d%d", &st,&len);
+for(k=st;k<(st+len);k++)
+if(f[k]==0)
+count++;
+if(len==count)
+{
+for(j=st;j<(st+len);j++)
+if(f[j]==0)
+{
+f[j]=1;
+printf("%d\t%d\n",j,f[j]);
+}
+if(j!=(st+len-1))
+printf("The file is allocated to disk\n");
+}
+else
+printf("The file is not allocated \n");
+printf("Do you want to enter more file(Yes - 1/No - 0)");
+scanf("%d", &c);
+if(c==1)
+goto x;
 }
